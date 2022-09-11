a3dc4bae85b873d76406812585298265d4873b93
Bladimiro Paredes <112091658+bladywalls@users.noreply.github.com>
2022-09-10 18:02:11 -0600

    Add files via upload

---
+++ lab02/celsius2farenheit.py
@@ -0,0 +1,8 @@
+#TEA
+temperaturaCelcius = float(input("Cual es la temperatura en Celcius? "))
+temperatureFahrenheit = (9/5) * temperaturaCelcius + 32
+
+message= "La temperatura en Fahrenheit es de: "
+message2= "°F"
+
+print(message, temperatureFahrenheit, message2)
---
+++ lab02/pagoxhora.py
@@ -0,0 +1,10 @@
+#TEA
+horas= input("Numero de horas a trabajar?")
+pago= input("Pago por hora a trabajar?")
+
+total= int(horas) * int(pago)
+
+message="dolares la hora"
+print(total, message)
+
+
